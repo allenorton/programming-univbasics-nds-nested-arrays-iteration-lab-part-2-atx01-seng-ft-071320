f91bd64b7f4a9b4e19acd6614fc31c4b60a3a923
Allene Norton <norton.allene@gmail.com>
2020-06-23 16:28:40 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,10 +6,7 @@ def find_min_in_nested_arrays(src)
   while row_index < src.count do
     el_index = 0 
     while el_index < src[row_index].count do
-      min_array << src[row_index].min
-      el_index += 1 
-    end
-    row_index += 1 
+      min_array << src[row_index].min    row_index += 1 
   end
   min_array
 end
